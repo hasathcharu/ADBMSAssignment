a7768a6093ea208d4269d075d36c01fe8ceda796
Haritha Hasathcharu <harithahasathcharu@icloud.com>
2023-08-27 16:46:56 +0530

    added apigateway, eureka server

--- OrderPlacement/src/main/java/org/adbms/orderplacement/OrderPlacementApplication.java
+++ OrderPlacement/src/main/java/org/adbms/orderplacement/OrderPlacementApplication.java
@@ -1,4 +1,4 @@
-package org.orderplacement;
+package org.adbms.orderplacement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
