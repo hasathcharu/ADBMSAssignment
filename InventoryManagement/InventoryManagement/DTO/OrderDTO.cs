bcae5f91cf71e1345fcf8c304a8945e79210c1a8
MWijesekara <menurawijesekara17@gmail.com>
2023-09-02 09:43:05 +0530

    After adding Inventory Management

---
+++ InventoryManagement/InventoryManagement/DTO/OrderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagement.DTO
+{
+    public class OrderDTO
+    {
+        public long PId { get; set; }
+        public double? Price { get; set; }
+        public double? Available_Quantity { get; set; }
+    }
+}
---
+++ InventoryManagement/InventoryManagement/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace InventoryManagement.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server= DESKTOP-JN3625D;Database=Inventory_DB;Trusted_Connection=true;TrustServerCertificate=true;");    
+        }
+
+        public DbSet<InventoryModel> Inventory { get; set; }
+    }
+}
